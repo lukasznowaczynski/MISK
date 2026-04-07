0b5842e51243d223edaee02e132ded073e507ccc
Your Name <you@example.com>
2026-04-07 19:32:16 +0200

    added model

---
+++ scripts/sim.py
@@ -0,0 +1,37 @@
+from coppeliasim_zmqremoteapi_client import RemoteAPIClient
+import os
+
+client = RemoteAPIClient()
+sim = client.getObject('sim')
+
+a_pwd = os.getcwd()
+model_path = a_pwd + "/models/rover.ttm"
+print(model_path)
+
+positions = [
+    [-2.0,  2.0, 0.4],
+    [ 2.0,  2.0, 0.4],
+    [-2.0, -2.0, 0.4],
+    [ 2.0, -2.0, 0.4]
+]
+
+names = ["rover_1", "rover_2", "rover_3", "rover_4"]
+
+base_handle = sim.loadModel(model_path)
+handles = [base_handle]
+
+# Copy models
+for _ in range(3):
+    copy = sim.copyPasteObjects([base_handle], 1)
+    handles.append(copy[0])
+
+# Move and rename
+for handle, pos, name in zip(handles, positions, names):
+    sim.setObjectPosition(handle, -1, pos)
+    sim.setObjectAlias(handle, name)  # 👈 rename root object
+
+sim.startSimulation()
+
+
+
+print("Rovers renamed and positioned")
